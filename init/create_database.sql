4a961fa3ffb7b70f43518e2ef57dfd98a5d8bcee
stepkos <stepkos@wp.pl>
2024-11-03 13:39:43 +0100

    Initialize database

---
+++ init/create_database.sql
@@ -0,0 +1,14 @@
+-- Database: student_cource_resource_managments
+
+-- DROP DATABASE IF EXISTS student_cource_resource_managments;
+
+CREATE DATABASE student_cource_resource_managments
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
