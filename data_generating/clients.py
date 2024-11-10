ef8e34046b94a56c3aad818e24b49183afd83e51
mkapica <mikolajkapica7@gmail.com>
2024-11-10 22:34:17 +0100

    ?

--- data_generating/clients.py
+++ data_generating/clients.py
@@ -8,30 +8,24 @@ password = values.get("POSTGRES_PASSWORD")
 dbname = values.get("POSTGRES_DBNAME")
 user = values.get("POSTGRES_USER")
 
-print(user, port, user, password)
+conn = psycopg.connect( f"host={host} port={port} dbname={dbname} user={user} password={password}")
 
+cur = conn.cursor()
+cur.execute(
+    """
+    CREATE TABLE test (
+        id serial PRIMARY KEY,
+        num integer,
+        data text)
+    """
+)
+cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
+cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
+cur.execute("SELECT * FROM test")
+cur.fetchone()
+for record in cur:
+    print(record)
 
-with psycopg.connect(
-    f"host={host} port={port} dbname={dbname} user={user} password={password}"
-) as conn:
-    with conn.cursor() as cur:
-        cur.execute(
-            """
-            CREATE TABLE test (
-                id serial PRIMARY KEY,
-                num integer,
-                data text)
-            """
-        )
+conn.commit()
 
-        cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
-
-        cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
-
-        cur.execute("SELECT * FROM test")
-        cur.fetchone()
-
-        for record in cur:
-            print(record)
-
-        conn.commit()
+conn.close()
