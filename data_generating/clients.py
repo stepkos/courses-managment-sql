8ac429952d9438ecc3ff623dd2bce6e9892e38d4
stepkos <stepkos@wp.pl>
2024-11-10 23:00:42 +0100

    feat: Update postgres client, requirements.txt

--- data_generating/clients.py
+++ data_generating/clients.py
@@ -1,12 +1,19 @@
 import psycopg
 from contextlib import contextmanager
 
+from data_generating import config
 
 
 @contextmanager
-def psql():
+def postgres_client():
 
-    conn = psycopg.connect(f"host={host} port={port} dbname={dbname} user={user} password={password}")
+    conn = psycopg.connect(
+        f"host={config.POSTGRES_HOST} "
+        f"port={config.POSTGRES_PORT} "
+        f"dbname={config.POSTGRES_PASSWORD} "
+        f"user={config.POSTGRES_DBNAME} "
+        f"password={config.POSTGRES_USER}"
+    )
 
     try:
         yield conn
