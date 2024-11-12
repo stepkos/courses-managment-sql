a954253d0965b2b3bf404d5ec6ed3ac2272750f0
mkapica <mikolajkapica7@gmail.com>
2024-11-12 18:55:19 +0100

    jedrek

--- data_generating/create_data.py
+++ data_generating/create_data.py
@@ -1,18 +1,55 @@
 from data_generating.seeders import *
 from data_generating.utils import generate_insert_query
 
-num_records = 1_000
+TERMS = 7
+AVG_GROUP_SIZE = 20
+AVG_STUDENT_LIFETIME = 6 #srednia liczba semestrow, na ktorej byl kazdzy student
+AVG_GROUPS_NUMBER_PER_COURSE = 7
+AVG_ENTRIES_PER_GROUP = 12
 
-degrees = generate_degree_seeder(num_records)
-users = generate_users_seeder(3 * num_records, list(degrees))
-administrators = list(filter(lambda x: x.profile_type == 0, users))
-hosts = list(filter(lambda x: x.profile_type == 1, users))
-students = list(filter(lambda x: x.profile_type == 2, users))
-student_and_hosts = students + hosts
+admins_number = 50
+faculties_number = 15
+admins_faculties_number = 90
+fields_of_study_number = 80
+terms_number = fields_of_study_number * 20
+courses_number = terms_number * 5
+college_terms_number = 20
+hosts_number = 1_800
+students_number = 40_000
+groups_number = courses_number * college_terms_number / TERMS * students_number / AVG_GROUP_SIZE / faculties_number
+students_groups_number = students_number * AVG_STUDENT_LIFETIME
+host_groups_number = int(groups_number * 1.2)
+hosts_courses_number = courses_number * AVG_GROUPS_NUMBER_PER_COURSE
+entries_number = courses_number * AVG_ENTRIES_PER_GROUP
+comments_of_entries_number = entries_number * 0.3
+entries_files_number = entries_number * 0.1
+exercises_number = entries_number * 0.95
+solutions_number = exercises_number * college_terms_number * AVG_GROUP_SIZE
 
-faculties = generate_faculty_seeder(num_records)
+
+
+def get_college_terms(n: int) -> List[CollegeTerm]:
+    terms = []
+    base_year = fake.date_this_century().year  # losowy rok początkowy, np. 2020
+
+    for i in range(n):
+        if i % 2 == 0:  # Semestr zimowy (parzyste indeksy)
+            start_date = f"{base_year + i // 2}-10-01"
+            end_date = f"{base_year + i // 2 + 1}-02-15"
+        else:  # Semestr letni (nieparzyste indeksy)
+            start_date = f"{base_year + i // 2 + 1}-02-16"
+            end_date = f"{base_year + i // 2 + 1}-06-30"
+
+        term = CollegeTerm(start_date=start_date, end_date=end_date)
+        terms.append(term)
+        return terms
+
+
+
+administrators = generate_administrator_seeder(admins_number)
+faculties = generate_faculty_seeder(faculties_number)
 faculty_administrators = generate_faculty_administrator_seeder(
-    num_records, faculties, administrators
+    admins_faculties_number, faculties, administrators
 )
 fields_of_study = generate_field_of_study_seeder(num_records, faculties, administrators)
 terms = generate_term_seeder(num_records, fields_of_study, administrators)
@@ -45,7 +82,6 @@ closed_answer_choices = generate_closed_answer_choice_seeder(
 
 
 tables: list[Sequence[BaseModel]] = [
-    degrees,
     administrators,
     faculties,
     faculty_administrators,
@@ -53,6 +89,7 @@ tables: list[Sequence[BaseModel]] = [
     terms,
     courses,
     college_terms,
+    degrees,
     hosts,
     groups,
     students,
