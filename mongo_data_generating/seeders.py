699ccf08f225ea1105db63330d616ea7606a4436
stepkos <stepkos@wp.pl>
2025-01-21 17:55:50 +0100

    refactor(factories): insert after all data generated

--- mongo_data_generating/seeders.py
+++ mongo_data_generating/seeders.py
@@ -13,11 +13,9 @@ def seed_data(multi: int = 1):
     res = faculty_factory()
     faculty = res["faculty"]
     courses_ids = res["all_courses_ids"]
-    GenericRepository("faculties").insert_one(faculty)
 
     users = [user_factory(courses_ids) for _ in range(2000 * multi)]
     users_ids = [user.id for user in users]
-    GenericRepository("users").insert_many(users)
 
     groups = [group_factory(courses_ids) for _ in range(50 * multi)]
     groups_ids = [group.id for group in groups]
@@ -25,24 +23,26 @@ def seed_data(multi: int = 1):
     for group in groups:
         students_ids.extend([s.student_id for s in group.students])
 
-    GenericRepository("groups").insert_many(groups)
-
     entries = [entry_factory(users_ids, groups_ids) for _ in range(100 * multi)]
     exercises_ids = [entry.exercise.exercise_id for entry in entries]
     tests_ids = [entry.test.test_id for entry in entries]
-    GenericRepository("entries").insert_many(entries)
 
     solutions = [
         solution_factory(users_ids, students_ids, exercises_ids) for _ in range(100 * multi)
     ]
-    GenericRepository("solutions").insert_many(solutions)
 
     attempts = [
         attempt_factory(students_ids, tests_ids)
         for _ in range(100 * multi)
     ]
+
+    GenericRepository("faculties").insert_one(faculty)
+    GenericRepository("users").insert_many(users)
+    GenericRepository("groups").insert_many(groups)
+    GenericRepository("entries").insert_many(entries)
+    GenericRepository("solutions").insert_many(solutions)
     GenericRepository("attempts").insert_many(attempts)
 
 
 if __name__ == "__main__":
-    seed_data()
+    seed_data(5)
